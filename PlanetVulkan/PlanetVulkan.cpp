bd8ed007d2d375fdbe7146ee43a85b59a38f337d
Darkchicken <joshshucker@hotmail.com>
2017-01-15 16:32:55 -0800

    Added Validation Layers
    Engine now enables validation layers for error checking
    Still has an error in PlanetVulkan.h, callback variable declared wrong

--- PlanetVulkan/PlanetVulkan.cpp
+++ PlanetVulkan/PlanetVulkan.cpp
@@ -8,6 +8,8 @@ Handles engine code
 #include <GLFW/glfw3.h>
 #include "PlanetVulkan.h"
 #include <vector>
+#include <cstring>
+
 namespace PlanetVulkanEngine
 {
 
@@ -23,14 +25,22 @@ namespace PlanetVulkanEngine
 	void PlanetVulkan::initVulkan()
 	{
 		createInstance();
+		setupDebugCallback();
 	}
 
 	void PlanetVulkan::createInstance()
 	{
+		//check for validation layers
+		if (enableValidationLayers && !checkValidationLayerSupport())
+		{
+			throw std::runtime_error("validation layers requested, but no available");
+		};
+
 		///set application info for Vulkan instance
 		VkApplicationInfo appInfo = {};
 		appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
-		appInfo.pApplicationName = "Hello Triangle";
+		appInfo.pNext = nullptr;
+		appInfo.pApplicationName = "Planet Vulkan";
 		appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
 		appInfo.pEngineName = "No Engine";
 		appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
@@ -39,19 +49,27 @@ namespace PlanetVulkanEngine
 		///Set which extenstions and validation layers to use
 		VkInstanceCreateInfo createInfo = {};
 		createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+		createInfo.pNext = nullptr;
+		createInfo.flags = 0;
 		createInfo.pApplicationInfo = &appInfo;
 
-		unsigned int glfwExtensionCount = 0;
-		const char** glfwExtensions;
-		//get the glfw extentions
-		glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
-		//set vulkan extentions to the glfw extentions
-		createInfo.enabledExtensionCount = glfwExtensionCount;
-		createInfo.ppEnabledExtensionNames = glfwExtensions;
-
-		createInfo.enabledLayerCount = 0;
+		if(enableValidationLayers)
+		{
+			createInfo.enabledLayerCount = validationLayers.size();
+			createInfo.ppEnabledLayerNames = validationLayers.data();
+		}
+		else
+		{
+			createInfo.enabledLayerCount = 0;
+			createInfo.ppEnabledLayerNames = nullptr;
+		}
+		
+		auto extensions = getRequiredExtensions();
+		createInfo.enabledExtensionCount = extensions.size();
+		createInfo.ppEnabledExtensionNames = extensions.data();
 
 		///check for extention support
+		/*
 		uint32_t extensionCount = 0;
 		vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
 		std::vector<VkExtensionProperties> extensions(extensionCount);
@@ -61,6 +79,7 @@ namespace PlanetVulkanEngine
 		{
 			std::cout << "\t" << extension.extensionName << std::endl;
 		}
+		*/
 
 		//Call to create Vulkan instance, replaces instance variable with created one. 
 		//Throws an error on failure
@@ -68,7 +87,108 @@ namespace PlanetVulkanEngine
 		{
 			throw std::runtime_error("failed to create instance!");
 		}
-		std::cout<< "Vulkan instance created"<<std::endl;
+		else
+		{
+			std::cout << "Vulkan instance created successfully" << std::endl;
+		}
+		
+	}
+
+	//returns false if any of the layers are unsupported
+	bool PlanetVulkan::checkValidationLayerSupport() 
+	{
+		uint32_t layerCount;
+		// 1st call gets number of layers
+		vkEnumerateInstanceLayerProperties(&layerCount, nullptr);
+
+		std::vector<VkLayerProperties> availableLayers(layerCount);
+		// second call stores all layers
+		vkEnumerateInstanceLayerProperties(&layerCount, availableLayers.data());
+		//iterate through all validation layers 
+		for (const char* layerName : validationLayers) 
+		{
+			bool layerFound = false;
+			//check if the layer is in available layers
+			for (const auto& layerProperties : availableLayers) 
+			{
+				if (strcmp(layerName, layerProperties.layerName) == 0) 
+				{
+					layerFound = true;
+					break;
+				}
+			}
+			if (!layerFound) 
+			{return false;}
+		}
+		return true;
+	}
+
+	//returns the required extensions, adds the callback extensions if enabled
+	std::vector<const char*> PlanetVulkan::getRequiredExtensions() 
+	{
+		std::vector<const char*> extensions;
+
+		unsigned int glfwExtensionCount = 0;
+		const char** glfwExtensions;
+		glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+		for (unsigned int i = 0; i < glfwExtensionCount; i++) 
+		{extensions.push_back(glfwExtensions[i]);}
+
+		if (enableValidationLayers) 
+		{extensions.push_back(VK_EXT_DEBUG_REPORT_EXTENSION_NAME);}
+
+		return extensions;
+	}
+
+	void PlanetVulkan::setupDebugCallback()
+	{
+		if (!enableValidationLayers) return;
+
+		VkDebugReportCallbackCreateInfoEXT createInfo = {};
+		createInfo.sType = VK_STRUCTURE_TYPE_DEBUG_REPORT_CALLBACK_CREATE_INFO_EXT;
+		createInfo.flags = VK_DEBUG_REPORT_ERROR_BIT_EXT | VK_DEBUG_REPORT_WARNING_BIT_EXT;
+		createInfo.pfnCallback = debugCallback;
+
+		if (CreateDebugReportCallbackEXT(instance, &createInfo, nullptr, callback.replace()) != VK_SUCCESS) 
+		{throw std::runtime_error("failed to set up debug callback!");}
+	}
+
+	VkResult PlanetVulkan::CreateDebugReportCallbackEXT(
+		VkInstance instance, 
+		const VkDebugReportCallbackCreateInfoEXT* pCreateInfo, 
+		const VkAllocationCallbacks* pAllocator, 
+		VkDebugReportCallbackEXT* pCallback) 
+	{
+		auto func = (PFN_vkCreateDebugReportCallbackEXT)vkGetInstanceProcAddr(instance, "vkCreateDebugReportCallbackEXT");
+		if (func != nullptr) 
+		{return func(instance, pCreateInfo, pAllocator, pCallback);}
+		else 
+		{return VK_ERROR_EXTENSION_NOT_PRESENT;}
+	}
+
+	void PlanetVulkan::DestroyDebugReportCallbackEXT(
+		VkInstance instance, 
+		VkDebugReportCallbackEXT callback, 
+		const VkAllocationCallbacks* pAllocator) 
+	{
+		auto func = (PFN_vkDestroyDebugReportCallbackEXT)vkGetInstanceProcAddr(instance, "vkDestroyDebugReportCallbackEXT");
+		if (func != nullptr) 
+		{func(instance, callback, pAllocator);}
+	}
+
+	VKAPI_ATTR VkBool32 VKAPI_CALL PlanetVulkan::debugCallback(
+		VkDebugReportFlagsEXT flags,
+		VkDebugReportObjectTypeEXT objType,
+		uint64_t obj,
+		size_t location,
+		int32_t code,
+		const char* layerPrefix,
+		const char* msg,
+		void* userData) 
+	{
+		std::cerr << "validation layer: " << msg << std::endl;
+		return VK_FALSE;
 	}
 }
 
--- PlanetVulkan/PlanetVulkan.h
+++ PlanetVulkan/PlanetVulkan.h
@@ -6,6 +6,7 @@ Copyright © 2017, Josh Shucker
 #include <vulkan/vulkan.h>
 #include <iostream>
 #include <stdexcept>
+#include <vector>
 
 #include "VDeleter.h"
 namespace PlanetVulkanEngine
@@ -20,9 +21,49 @@ namespace PlanetVulkanEngine
 
 
 	private:
+
 		void createInstance();
 
+		bool checkValidationLayerSupport();
+
+		std::vector<const char*> getRequiredExtensions();
+
+		void setupDebugCallback();
+
+		VkResult CreateDebugReportCallbackEXT(
+			VkInstance instance,
+			const VkDebugReportCallbackCreateInfoEXT* pCreateInfo,
+			const VkAllocationCallbacks* pAllocator,
+			VkDebugReportCallbackEXT* pCallback);
+
+		void DestroyDebugReportCallbackEXT(
+			VkInstance instance, 
+			VkDebugReportCallbackEXT callback, 
+			const VkAllocationCallbacks* pAllocator);
+
+		static VKAPI_ATTR VkBool32 VKAPI_CALL debugCallback(
+			VkDebugReportFlagsEXT flags,
+			VkDebugReportObjectTypeEXT objType,
+			uint64_t obj,
+			size_t location,
+			int32_t code,
+			const char* layerPrefix,
+			const char* msg,
+			void* userData);
+
 		VDeleter<VkInstance> instance{ vkDestroyInstance };
+	
+		VDeleter<VkDebugReportCallbackEXT> callback{instance, DestroyDebugReportCallbackEXT};
+
+		///Validation Layer variables
+		const std::vector<const char*> validationLayers = {"VK_LAYER_LUNARG_standard_validation"};
+
+#ifdef NDEBUG
+		const bool enableValidationLayers = false;
+#else
+		const bool enableValidationLayers = true;
+#endif // NDEBUG
+
 	};
 }
 
